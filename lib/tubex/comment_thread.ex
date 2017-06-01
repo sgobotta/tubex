b981faaef5951ffd64ce637fa350c7386a388664
David Sison <david@metismachine.com>
2017-06-01 10:38:27 -0400

    Add CommentThread by video ID function

--- lib/tubex/comment_thread.ex
+++ lib/tubex/comment_thread.ex
@@ -13,4 +13,17 @@ defmodule Tubex.CommentThread do
       err -> err
     end
   end
+
+  def by_video(video_id, opts \\ []) do
+    defaults = [key: Tubex.api_key, videoId: video_id, maxResults: 50, part: "replies,snippet"]
+    opts = Keyword.merge(defaults, opts)
+
+    case Tubex.API.get(Tubex.endpoint <> "/commentThreads", opts) do
+      {:ok, response} ->
+        tokens = %{"nextPageToken" => response["nextPageToken"], "prevPageToken" => response["prevPageToken"]}
+        page_info = Map.merge(response["pageInfo"], tokens)
+        {:ok, response["items"], page_info}
+      err -> err
+    end
+  end
 end
