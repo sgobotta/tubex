46a6f20a35f805726b49f4f8e06db5e12567866d
Santiago Botta <sgobotta@gmail.com>
2021-02-28 13:54:15 -0300

    :test_tube: Tests the search_by_query function returns a video response

--- test/tubex_test.exs
+++ test/tubex_test.exs
@@ -1,8 +1,18 @@
 defmodule TubexTest do
   use ExUnit.Case
+
   doctest Tubex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Tubex.Video
+
+  describe "API - Search by query" do
+    test "GET - A list of videos is returned on search_by_query" do
+      search_query = "mandatory param"
+      opts = [maxResults: 40]
+      {:ok, response, page_info} = Video.search_by_query(search_query, opts)
+
+      assert Keyword.fetch!(opts, :maxResults) == length(response)
+      assert Keyword.fetch!(opts, :maxResults) == page_info["resultsPerPage"]
+    end
   end
 end
